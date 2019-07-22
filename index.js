90c42ed31e47d82e21930aed15cb9033d9122d33
Andrew Zajac <ajzajac@icloud.com>
2019-07-22 20:54:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 function nestedTarget(){
-  var i = document.querySelector('div.nested')
+ return document.querySelector('#nested .target')
 }
