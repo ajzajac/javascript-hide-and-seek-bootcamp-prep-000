cf8ace2dfe5a2d8fb2084dbaa61b42199864a4d6
Andrew Zajac <ajzajac@icloud.com>
2019-07-22 20:42:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 function nestedTarget(){
-  return document.getElementsBy
+  return document.getElementsByClassName('')
 }
